888977a9c61b7f1a964017cb5abf91ba58a72843
Dxd4 <46905365+Dxd4@users.noreply.github.com>
2022-09-28 11:08:34 +0300

    Update Main.java

--- File_to_byte_array/Main.java
+++ File_to_byte_array/Main.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Read_file read = new Read_file();
-        byte[] file = read.read("src/File_to_byte_array/Test.txt");
+        Scanner scanner = new Scanner(System.in);
+        byte[] file = read.read(scanner.nextLine());
     }
 }
